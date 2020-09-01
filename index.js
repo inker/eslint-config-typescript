481e597b2da16fb98189645459bb491ec4a3767f
inker <anton.veselyev@gmail.com>
2020-09-01 04:03:57 +0100

    update

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 0,
     '@typescript-eslint/ban-ts-ignore': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
     '@typescript-eslint/member-delimiter-style': [2, {
